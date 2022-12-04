061e4d1f2ae3555711712c3e42b7bbec9d27f27d
Erin Dar <erin.malone76@yahoo.com>
2022-12-03 19:18:00 -0500

    switching to react-quilljs for editor

--- react-app/src/components/Editor.js
+++ react-app/src/components/Editor.js
@@ -6,6 +6,7 @@ export default function Editor() {
     const [value, setValue] = useState('');
 
     return (
-        <ReactQuill theme="snow" value={value} onChange={setValue} />
+        <>
+        </>
     );
 }
